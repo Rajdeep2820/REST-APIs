533bc82763bb111479e8209fb42eb0bf5106d32b
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-06-19 18:52:44 +0530

    final commit

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const methodOverride = require("method-override")
 
 app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"))
 app.set("view engine" , "ejs");
 app.set(path.join(__dirname, "views")); 
 
@@ -47,6 +48,28 @@ app.get("/posts/:id" , (req,res)=>{
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs" , {post});
 });
+ 
+app.patch("/posts/:id" , (req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.data;
+    console.log(newContent);
+    console.log(id);
+    let post = posts.find((p)=> id === p.id);
+    post.data = newContent;
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/Edit" , (req,res)=> {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post} );
+})
+
+app.delete("/posts/:id" , (req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts"); 
+})
 
 const port = 3030;
 app.listen(port, () => {
